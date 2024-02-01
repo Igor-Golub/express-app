f78197972432f953f62fcb192667b037c99935c3
igorGolub <gitihonovich@gmail.com>
2024-02-01 17:09:36 +0300

    Changed routs.

--- src/enums/Routs.ts
+++ src/enums/Routs.ts
@@ -1,5 +1,5 @@
 export enum Routs {
-  Videos = "/hometask_01/api/videos",
-  VideosWithId = "/hometask_01/api/videos/:id",
-  Testing = "/hometask_01/api/testing/all-data",
+  Videos = "/videos",
+  VideosWithId = "/videos/:id",
+  Testing = "/testing/all-data",
 }
