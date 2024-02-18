59f6ba54cf9ce6f3a68e6bb6df7ae927b82c9601
igorGolub <gitihonovich@gmail.com>
2024-02-18 11:51:04 +0300

    Added tsc and prettier scripts.

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -1,18 +1,19 @@
 import { NextFunction, Request, Response } from "express";
-import {  validationResult } from "express-validator";
+import { validationResult } from "express-validator";
 
 const convertErrorToContract = (errors: Record<string, { msg: string }>) => {
   return {
     errorsMessages: Object.entries(errors).map(([field, { msg }]) => ({
       field,
-      message: msg
-    }))
-  }
-}
+      message: msg,
+    })),
+  };
+};
 
 export const validation = (req: Request, res: Response, next: NextFunction) => {
-  const result = validationResult(req)
+  const result = validationResult(req);
 
-  if (!result.isEmpty()) res.status(400).send(convertErrorToContract(result.mapped()))
-  else next()
-}
+  if (!result.isEmpty())
+    res.status(400).send(convertErrorToContract(result.mapped()));
+  else next();
+};
