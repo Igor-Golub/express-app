2ba384e5f80ca6947835c01b90eb1286e2c5d056
igorGolub <gitihonovich@gmail.com>
2024-02-29 12:35:13 +0300

    Fix enum.

--- src/enums/StatusCodes.ts
+++ src/enums/StatusCodes.ts
@@ -2,7 +2,7 @@ export enum StatusCodes {
   Ok_200 = 200,
   Created_201 = 201,
   NoContent_204 = 204,
-  Unauthorized_401 = 400,
-  BadRequest_400 = 401,
+  Unauthorized_401 = 401,
+  BadRequest_400 = 400,
   NotFound_404 = 404,
 }
