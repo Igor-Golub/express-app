218e5ef44b9d3b26552f6e309619e933f17f46cc
igorGolub <gitihonovich@gmail.com>
2024-04-07 12:46:31 +0300

    Added recovery template message.

--- src/application/notification/email/EmailTemplatesCreator.ts
+++ src/application/notification/email/EmailTemplatesCreator.ts
@@ -3,8 +3,17 @@ class EmailTemplatesCreator {
     return `
         <h1>Thank ${login} for your registration</h1>
         <p>
-            To finish registration please follow the link below:
-            <a href='https://somesite.com/confirm-email?code=${code}'>complete registration</a>
+          To finish registration please follow the link below:
+          <a href='https://somesite.com/confirm-email?code=${code}'>complete registration</a>
+        </p>
+        `;
+  }
+
+  public getRecoveryTemplate(login: string, code: string) {
+    return `
+        <h1>Password recovery for, ${login}</h1>
+          <p>To finish password recovery please follow the link below:
+          <a href='https://somesite.com/password-recovery?recoveryCode=${code}'>recovery password</a>
         </p>
         `;
   }
--- src/managers/NotifyManager.ts
+++ src/managers/NotifyManager.ts
@@ -15,6 +15,15 @@ class NotifyManager {
       template: this.emailTemplatesCreator.getRegistrationTemplate(login, code),
     });
   }
+
+  public async sendRecoveryEmail(login: string, email: string, code: string) {
+    return this.emailService.sendEmail({
+      from: "<MEGA service>",
+      address: email,
+      subject: "Recovery password",
+      template: this.emailTemplatesCreator.getRecoveryTemplate(login, code),
+    });
+  }
 }
 
 export default new NotifyManager(NotifyService, EmailTemplatesCreator);
